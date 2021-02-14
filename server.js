0156b780bd8acc7e87cac5c20695df4bb362407a
JasonDollar <elrey33@gmail.com>
2021-02-14 11:33:10 +0100

    cors isuses

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 3080
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use('/api/v1/expenses', expenseRoutes)
 app.use('/api/v1/users', userRoutes)
 
