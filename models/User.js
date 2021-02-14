5aab7a0a1cfff7f22d2f20931668e6c5e078d80e
JasonDollar <elrey33@gmail.com>
2021-02-14 10:25:44 +0100

    user model

---
+++ models/User.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, add a name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please, add an email'],
+    trim: true,
+    lowercase: true,
+    match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
+      'Please add a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please, provide a password'],
+    minlength: [8, 'Password need to be at least 8 characters long'],
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"')
+      }
+    },
+  },
+
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
+  avatar: {
+    type: Buffer,
+  },
+}, {
+  timestamps: true,
+})
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
