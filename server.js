da334f8390aa2c6f48df0cb3a82bf6940a560f37
JasonDollar <elrey33@gmail.com>
2021-02-14 17:26:23 +0100

    added cors package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config({ path: '.env' })
 const express = require('express')
+const cors = require('cors')
 const connectDB = require('./config/db')
 const expenseRoutes = require('./routes/expenses')
 const userRoutes = require('./routes/users')
@@ -10,11 +11,7 @@ const port = process.env.PORT || 3080
 
 app.use(express.json())
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+app.use(cors())
 
 app.use('/api/v1/expenses', expenseRoutes)
 app.use('/api/v1/users', userRoutes)
