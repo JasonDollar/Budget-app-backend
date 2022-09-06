7242d59f61d765682a7efc053fd842abec918562
JasonDollar <elrey33@gmail.com>
2022-09-06 19:12:55 +0200

    server serves app from public folder

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ connectDB()
 const port = process.env.PORT || 3080
 
 app.use(express.json())
+app.use(express.static('public'))
 
 app.use(cors())
 
 app.use('/api/v1/expenses', expenseRoutes)
 app.use('/api/v1/users', userRoutes)
 
-app.get('/', (req, res) => {
-  res.send('App Works !!!!')
+app.get('*', (req, res) => {
+  res.send('/public/index.html')
 })
 
 app.use(errorHandler)
